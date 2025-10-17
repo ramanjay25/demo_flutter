c65b67dac331df8cb2b54c43250088328566dcf5
LearningFlutter373 <155526378+LearningFlutter373@users.noreply.github.com>
2025-10-17 15:11:21 +0530

    i  added a signup screen in this app.

---
+++ lib/signup.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Signup Screen"),
+      ),
+    );
+  }
+}
